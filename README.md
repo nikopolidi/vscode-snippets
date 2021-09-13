edb504088c3de00037adcbd39d60522acc909a4c
Vitaly Nikopolidi <v.nikopolidi@gmail.com>
2021-09-13 15:49:13 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# vscode-snippets
+
+Snippets for vs-code
+
+# Installation:
+
+- Mac OS: `sh install.sh` or move all files to `~/Library/Application\ Support/Code/User/snippets`
+- any other: `move to /snippets folder`
---
+++ install.sh
@@ -0,0 +1,8 @@
+rm -f ~/Library/Application\ Support/Code/User/snippets/RN-typescript.code-snippets		
+rm -f ~/Library/Application\ Support/Code/User/snippets/map.code-snippets
+rm -f ~/Library/Application\ Support/Code/User/snippets/flowjs.code-snippets			
+rm -f ~/Library/Application\ Support/Code/User/snippets/material-ui.code-snippets
+rm -f ~/Library/Application\ Support/Code/User/snippets/js-react.code-snippets			
+rm -f ~/Library/Application\ Support/Code/User/snippets/react-native-snippets.code-snippets
+
+cp ./*.code-snippets ~/Library/Application\ Support/Code/User/snippets/
